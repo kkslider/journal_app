1232643294911d15b826571283ed0cf1a167595e
Benjamin Lee <benjamsandwich@gmail.com>
2013-10-30 11:54:50 -0700

    Allow dynamic editing of post title/body, save not yet implemented

--- app/assets/javascripts/views/posts/posts_show.js
+++ app/assets/javascripts/views/posts/posts_show.js
@@ -1,9 +1,10 @@
 JournalApp.Views.PostsShow = Backbone.View.extend({
   template: JST["posts/show"],
     
-  // events: {
-  //   "dblclick "
-  // },
+  events: {
+    "dblclick .user-input": "editField",
+    "blur .user-input": "saveField",
+  },
   
   render: function() {
     var renderedContent = this.template({
@@ -13,6 +14,23 @@ JournalApp.Views.PostsShow = Backbone.View.extend({
     
     this.$el.html(renderedContent);
     return this;
+  },
+    
+  editField: function(event) {
+    var currentText = event.currentTarget.textContent;
+    $(event.currentTarget).replaceWith("<input class='user-input'></input");
+    $(".update").val(currentText);
+  },
+  
+  saveField: function(event) {
+    var formData = $("#edit-form").serializeJSON();
+    this.model.set(formData);
+    this.model.save({}, {
+      success: function() {
+        console.log("SUCCESS!!");
+        Backbone.history.navigate("/", { trigger: true });
+      }
+    });
   }
   
   
